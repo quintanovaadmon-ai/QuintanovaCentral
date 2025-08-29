5f96752a10d85aaa51a4ca4f5741b46fcec05701
quintanovaadmon-ai <quintanovaadmon@gmail.com>
2025-08-29 00:28:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -138,7 +138,6 @@ def logs():
         pass
     return jsonify(registros)
 
-
+# Ejecutar la app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
-
